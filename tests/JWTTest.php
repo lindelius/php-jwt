1e59456003b95f9e60bc70bf220cc7c1c1ed13e6
Tom Lindelius <tom.lindelius@gmail.com>
2017-02-25 16:12:02 +0100

    Added test files

---
+++ tests/JWTTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lindelius\JWT\Tests;
+
+use Lindelius\JWT\JWT;
+
+/**
+ * Class JWTTest
+ *
+ * @author  Tom Lindelius <tom.lindelius@gmail.com>
+ * @version 2017-02-25
+ */
+class JWTTest extends TestCase
+{
+
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lindelius\JWT\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase
+ *
+ * @author  Tom Lindelius <tom.lindelius@gmail.com>
+ * @version 2017-02-25
+ */
+class TestCase extends BaseTestCase
+{
+}
