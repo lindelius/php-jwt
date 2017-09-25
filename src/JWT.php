8438a029f9d366827151e8e4a090b9c436403a41
Tom Lindelius <tom.lindelius@gmail.com>
2017-09-25 10:44:47 +0200

    Added support for RS384 and RS512 algorithms

--- src/JWT.php
+++ src/JWT.php
@@ -88,6 +88,8 @@ class JWT implements Iterator
         'HS512' => ['hash_hmac', 'SHA512'],
         'HS384' => ['hash_hmac', 'SHA384'],
         'RS256' => ['openssl', 'SHA256'],
+        'RS384' => ['openssl', 'SHA384'],
+        'RS512' => ['openssl', 'SHA512'],
     ];
 
     /**
