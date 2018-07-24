96a35a2915bc212ffb0bee32cc88301161803b91
Tom Lindelius <tom.lindelius@gmail.com>
2018-07-24 23:02:33 +0200

    Added unit tests for the aud claim validation

---
+++ tests/AudClaimTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Lindelius\JWT\Tests;
+
+use Lindelius\JWT\JWT;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AudClaimTest
+ *
+ * @author  Tom Lindelius <tom.lindelius@gmail.com>
+ * @version 2018-07-24
+ */
+class AudClaimTest extends TestCase
+{
+    /**
+     * The value of the "aud" claim, if included, MUST be either a string or an
+     * array of strings.
+     *
+     * @return array
+     */
+    public function invalidAudienceProvider()
+    {
+        return [
+
+            [1],
+            [0.07],
+            [new \stdClass()],
+            [false],
+
+            [[1]],
+            [[0.07]],
+            [[null]],
+            [[new \stdClass()]],
+            [[false]],
+            [[[]]],
+            [[7 => 'value']],
+            [['key' => 'value']],
+
+        ];
+    }
+
+    /**
+     * @param mixed $aud
+     * @expectedException \Lindelius\JWT\Exception\InvalidJwtException
+     * @expectedExceptionMessage Invalid "aud" value.
+     * @dataProvider             invalidAudienceProvider
+     */
+    public function testDecodeWithInvalidAudValue($aud)
+    {
+        $jwt = new JWT('HS256');
+
+        $jwt->aud = $aud; // Set an invalid "aud" value
+
+        $jwt->encode('my_key');
+        $jwt->verify('my_key', 'https://myapp.tld');
+    }
+
+    /**
+     * @expectedException \Lindelius\JWT\Exception\InvalidAudienceException
+     * @throws \Lindelius\JWT\Exception\InvalidJwtException
+     */
+    public function testDecodeWithInvalidAudience()
+    {
+        $decodedJwt = JWT::decode('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczpcL1wvbXlhcHAudGxkIn0.kfXUmztf59REc6YAHNS7J1SleE_ufiWK7bTgSqM_buo');
+        $decodedJwt->verify('my_key', 'https://unknownapp.tld');
+    }
+
+    /**
+     * @expectedException \Lindelius\JWT\Exception\InvalidAudienceException
+     * @throws \Lindelius\JWT\Exception\InvalidJwtException
+     */
+    public function testDecodeWithInvalidAudienceAmongSeveral()
+    {
+        $decodedJwt = JWT::decode('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsiaHR0cHM6XC9cL215YXBwLnRsZCIsImh0dHBzOlwvXC95b3VyYXBwLnRsZCJdfQ.Yxa674OTihi3i3pp00DEa_BAPMmcIgTwQmbEaN-sNfA');
+        $decodedJwt->verify('my_key', 'https://unknownapp.tld');
+    }
+
+    /**
+     * @throws \Lindelius\JWT\Exception\InvalidJwtException
+     */
+    public function testDecodeWithValidAudience()
+    {
+        $decodedJwt = JWT::decode('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczpcL1wvbXlhcHAudGxkIn0.kfXUmztf59REc6YAHNS7J1SleE_ufiWK7bTgSqM_buo');
+
+        $this->assertEquals(
+            true,
+            $decodedJwt->verify('my_key', 'https://myapp.tld')
+        );
+    }
+
+    /**
+     * @throws \Lindelius\JWT\Exception\InvalidJwtException
+     */
+    public function testDecodeWithValidAudienceAmongSeveral()
+    {
+        $decodedJwt = JWT::decode('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsiaHR0cHM6XC9cL215YXBwLnRsZCIsImh0dHBzOlwvXC95b3VyYXBwLnRsZCJdfQ.Yxa674OTihi3i3pp00DEa_BAPMmcIgTwQmbEaN-sNfA');
+
+        $this->assertEquals(
+            true,
+            $decodedJwt->verify('my_key', 'https://myapp.tld')
+        );
+    }
+}
